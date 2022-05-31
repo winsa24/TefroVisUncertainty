56db2190471a13f94088177f5f27a6fef980e08e
winsa24 <winsaaa24@gmail.com>
2022-05-31 12:15:23 +0200

    show image on map
    fix size and import errors on ipynb
    
    iterate volcan glyph
    
    heatmap bins adapt to zoom level
    
    zoom adaptive volcano triangle

--- public/js/tef-map.js
+++ public/js/tef-map.js
@@ -8,6 +8,8 @@ export default function map() {
   var _samples
   var _tef
 
+  let bins = 10;
+
   // INITIALIZATION
   map.init = function (tef) {
 
@@ -28,11 +30,56 @@ export default function map() {
     _mapContainer = L.map('volcanoMap').setView([-45, -71.489618], 6)
     grayscale.addTo(_mapContainer)
     L.control.scale().addTo(_mapContainer)
+    _mapContainer.on('zoomend',function(e){
+      console.log(_mapContainer.getZoom())
+      let zoomLevel = _mapContainer.getZoom()
+      if(zoomLevel <= 3) bins = 2;
+      else if(zoomLevel > 3 && zoomLevel <= 6) bins = 10;
+      else if(zoomLevel > 6 && zoomLevel <= 8) bins = 20;
+      else if(zoomLevel > 8 && zoomLevel <= 10) bins = 50;
+      else bins = 100;
+  
+      removeOldIms()
+      addNewIms()
+    })
+
     _volcanes = {}
     _samples = {}
     return map
   }
+
+  
+  
+  let volcanIms = []
+  function removeOldIms(){
+    volcanIms.forEach(function (m) {
+      _mapContainer.removeLayer(m)
+    })
+  }
+  function addNewIms(){
+    console.log(_volcanes)
+    for (const [volcanName, volcan] of Object.entries(_volcanes)) {
+      // [0.12, 0.2]=> put image org at the triangle top
+      let lat = Number(volcan._latlng.lat) + 0.12
+      let lon =  Number(volcan._latlng.lng) + 0.2
+      // var lat = Number(volcan._bounds._northEast.lat)
+      // var lon = Number(volcan._bounds._northEast.lng)
+      var diff = 0.05
+
+      var imageUrl = `/img/heatmap_${bins}_Reds/${volcanName}.png`
+      if(['Huanquihue Group', 'Carrán-Los Venados', 'Yanteles', 'Viedma'].indexOf(volcanName) >= 0)  imageUrl = `/img/heatmap_2_Reds/Corcovado.png`
+      if(['Puntiagudo', 'Tronador', 'Arenales', 'Aguilera', 'Reclus', 'Fueguino', 'Monte Burney'].indexOf(volcanName) >= 0)  imageUrl = `/img/heatmap_2_Reds/Corcovado.png`
+      var imageBounds = [[lat + diff * 2, lon + diff * 4], [lat - diff * 2, lon - diff * 4]]
+      let volcanIm = L.imageOverlay(imageUrl, imageBounds, {alt: `no plot for ${volcanName}`}).addTo(_mapContainer)
+      volcanIms.push(volcanIm)
+    }
+  }
   map.addVolcanoes = function (volcanes) {
+    console.log(volcanes)
+    var cmReds = d3.scaleQuantize()
+    .domain([0,500])// TODO:: normlize volcan sample number
+    .range(["#fff5f0","#fff4ef","#fff4ee","#fff3ed","#fff2ec","#fff2eb","#fff1ea","#fff0e9","#fff0e8","#ffefe7","#ffeee6","#ffeee6","#ffede5","#ffece4","#ffece3","#ffebe2","#feeae1","#fee9e0","#fee9de","#fee8dd","#fee7dc","#fee6db","#fee6da","#fee5d9","#fee4d8","#fee3d7","#fee2d6","#fee2d5","#fee1d4","#fee0d2","#fedfd1","#feded0","#feddcf","#fedccd","#fedbcc","#fedacb","#fed9ca","#fed8c8","#fed7c7","#fdd6c6","#fdd5c4","#fdd4c3","#fdd3c1","#fdd2c0","#fdd1bf","#fdd0bd","#fdcfbc","#fdceba","#fdcdb9","#fdccb7","#fdcbb6","#fdc9b4","#fdc8b3","#fdc7b2","#fdc6b0","#fdc5af","#fdc4ad","#fdc2ac","#fdc1aa","#fdc0a8","#fcbfa7","#fcbea5","#fcbca4","#fcbba2","#fcbaa1","#fcb99f","#fcb89e","#fcb69c","#fcb59b","#fcb499","#fcb398","#fcb196","#fcb095","#fcaf94","#fcae92","#fcac91","#fcab8f","#fcaa8e","#fca98c","#fca78b","#fca689","#fca588","#fca486","#fca285","#fca183","#fca082","#fc9e81","#fc9d7f","#fc9c7e","#fc9b7c","#fc997b","#fc987a","#fc9778","#fc9677","#fc9475","#fc9374","#fc9273","#fc9071","#fc8f70","#fc8e6f","#fc8d6d","#fc8b6c","#fc8a6b","#fc8969","#fc8868","#fc8667","#fc8565","#fc8464","#fb8263","#fb8162","#fb8060","#fb7f5f","#fb7d5e","#fb7c5d","#fb7b5b","#fb795a","#fb7859","#fb7758","#fb7657","#fb7455","#fa7354","#fa7253","#fa7052","#fa6f51","#fa6e50","#fa6c4e","#f96b4d","#f96a4c","#f9684b","#f9674a","#f96549","#f86448","#f86347","#f86146","#f86045","#f75e44","#f75d43","#f75c42","#f65a41","#f65940","#f6573f","#f5563e","#f5553d","#f4533c","#f4523b","#f4503a","#f34f39","#f34e38","#f24c37","#f24b37","#f14936","#f14835","#f04734","#ef4533","#ef4433","#ee4332","#ed4131","#ed4030","#ec3f2f","#eb3d2f","#eb3c2e","#ea3b2d","#e93a2d","#e8382c","#e7372b","#e6362b","#e6352a","#e5342a","#e43229","#e33128","#e23028","#e12f27","#e02e27","#df2d26","#de2c26","#dd2b25","#dc2a25","#db2924","#da2824","#d92723","#d72623","#d62522","#d52422","#d42321","#d32221","#d22121","#d12020","#d01f20","#ce1f1f","#cd1e1f","#cc1d1f","#cb1d1e","#ca1c1e","#c91b1e","#c71b1d","#c61a1d","#c5191d","#c4191c","#c3181c","#c2181c","#c0171b","#bf171b","#be161b","#bd161a","#bb151a","#ba151a","#b91419","#b81419","#b61419","#b51319","#b41318","#b21218","#b11218","#b01218","#ae1117","#ad1117","#ac1117","#aa1017","#a91016","#a71016","#a60f16","#a40f16","#a30e15","#a10e15","#a00e15","#9e0d15","#9c0d14","#9b0c14","#990c14","#970c14","#960b13","#940b13","#920a13","#900a13","#8f0a12","#8d0912","#8b0912","#890812","#870811","#860711","#840711","#820711","#800610","#7e0610","#7c0510","#7a0510","#78040f","#76040f","#75030f","#73030f","#71020e","#6f020e","#6d010e","#6b010e","#69000d","#67000d"]);
+    
     volcanes.forEach(function (volcan, i) {
       var lat = Number(volcan.Latitude)
       var lon = Number(volcan.Longitude)
@@ -46,7 +93,18 @@ export default function map() {
         [lat - diff, lon + diff],
         [lat + diff, lon],
       ]
-      var volcanIcon = L.polygon(latlngs, { color: 'grey', fillOpacity: 1 })
+
+      let sampleNumbers = (volcan.bulk_pyroclastic_regression_SampleNumber!=-1?volcan.bulk_pyroclastic_regression_SampleNumber:0 )+ (volcan.effusive_regression_SampleNumber!=-1?volcan.effusive_regression_SampleNumber:0 )
+      // var volcanIcon = L.polygon(latlngs, { color: 'grey', fillOpacity: 0.7, opacity: 0.7 })
+      let latLng = L.latLng(lat, lon)
+      var volcanIcon = L.triangleMarker(latLng, {
+        rotation: 0,
+        width: 20,
+        height: 20,
+        color: cmReds(sampleNumbers),
+        fillColor: cmReds(sampleNumbers)
+      })
+
       volcanIcon.id = volcan.Name
       volcanIcon.color = volcan.Color
       volcanIcon.isSelected = false
@@ -63,9 +121,36 @@ export default function map() {
       volcanIcon.on('mouseout', function (e) {
         this.closePopup()
       })
+
+      // scatter + cluster + border => `/img/scatter_plots_ border/${volcan.Name}.png`
+      // scatter + cluster + fill(alpha) => `scatter_plots_fill_border_alpha_sp`
+      // cluster + fill => `scatter_plots_fill_border_alpha_nsp`
+      // scatter + cluster + fill(solid) => `scatter_plots_fill_border_solid`
+      // scatter + cluster + fill(alpha = norm(sample number) ) => `scatter_plots_fill_border_alpha_sp_notr`
+      // scatter + cluster + fill(alpha = norm(sample number/area) )=> `scatter_plots_fill_border_alpha_sp_noareatr`
+      // scatter(GT) + cluster(in 3 group) + fill(alpha) + RL + info + white bgk => `/img/scatter_plots_fillPolygon_RL_info_whitebgk/${volcan.Name}_trend.png`
+      // scatter(GT) + cluster(in 3 group) + fill(alpha) + RL => `/img/scatter_plots_fillPolygon_RL/${volcan.Name}_trend.png`
+      // scatter(GT) + cluster + fill(alpha) + RL => `/img/scatter_plots_fillPolygon_RL_allCluster/${volcan.Name}_trend.png`
+
+      // cluster + fill(grayscale(allsampleNumbers)) + border(Color) + RL(1 wrt case) => `/img/scatter_plots_fillPolygon_RLcase_allCluster_allgrayscale/${volcan.Name}.png`
+      // cluster + fill(alpha) + RL(1 wrt case) => `/img/scatter_plots_fillPolygon_RLcase_allCluster/${volcan.Name}.png`
+      // cluster(on 3 group) + fill(alpha) + RL(1 wrt case) =>  `/img/scatter_plots_fillPolygon_RLcase_groupCluster/${volcan.Name}.png`
+      // group + fill(alpha) + RL(1 wrt case) =>  `/img/scatter_plots_fillPolygon_RLcase_group/${volcan.Name}.png`
+      // group + fill(grayscale(allsampleNumbers)) + RL(1 wrt case) =>  `/img/scatter_plots_fillPolygon_RLcase_group/${volcan.Name}.png`
+      // heatmap => heatmap
+      // scatter(glass) + cluster + fill(alpha) + RL(1 wrt case) => `/img/scatter_plots_fillPolygon_RLcase_allCluster_scatterGlass/${volcan.Name}.png`
+
+
+      // var imageUrl = `/img/heatmap_${bins}_r/${volcan.Name}.png`
+      // if(['Huanquihue Group', 'Carrán-Los Venados', 'Yanteles', 'Viedma'].indexOf(volcan.Name) >= 0)  imageUrl = `/img/scatter_plots_fillPolygon_RLcase_allCluster_allgrayscale/Corcovado.png`
+      // if(['Puntiagudo', 'Tronador', 'Arenales', 'Aguilera', 'Reclus', 'Fueguino', 'Monte Burney'].indexOf(volcan.Name) >= 0)  imageUrl = `/img/scatter_plots_fillPolygon_RLcase_allCluster_allgrayscale/Corcovado.png`
+      // var imageBounds = [[lat + diff * 2, lon + diff * 4], [lat - diff * 2, lon - diff * 4]]
+      // L.imageOverlay(imageUrl, imageBounds, {alt: `no plot for ${volcan.Name}`}).addTo(_mapContainer)
+
       _volcanes[volcan.Name] = volcanIcon
       _samples[volcan.Name] = []
     })
+    addNewIms()
     return map
   }
 
